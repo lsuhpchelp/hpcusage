a4a7f5488c50ac4e96f664cd02b460e999c8c7a1
chenyuetian <chenyuetian1984@gmail.com>
2019-06-19 21:12:03 -0500

    .

--- hpcUsageGetUser.R
+++ hpcUsageGetUser.R
@@ -9,11 +9,16 @@ fdump <- paste0(substr(prefix,1,nchar(prefix)-1),"dump.txt")
 # Read data from the LDAP dump.
 ldump <- readLines(fdump)
 lstdump <- sapply(ldump,function(x) strsplit(x,'|',fixed=T))
+
+options(stringsAsFactors = FALSE, row.names = FALSE)
 d <- do.call(rbind.data.frame, lstdump)
+row.names(d) <- c()
 
 # Keep only uid, name and email of users
-ud <- d[,c(1,5,10)]
-colnames(ud) <- c("user","name","email")
+#ud <- d[,c(1,5,10)]
+#colnames(ud) <- c("user","name","email")
+ud <- d[,c(1,2,5,8,10,12,17,18)]
+colnames(ud) <- c("user","status","name","group","email","sponsor","ctime","mtime")
 
 # Use email to get instituion.
 ud$org <- tolower(unlist(lapply(strsplit(ud$email,"@|\\."),function(x) rev(x)[2])))
--- hpcUsagePreprocess.R
+++ hpcUsagePreprocess.R
@@ -1,3 +1,26 @@
+# change on 6/19/2019:
+# 1. In the section of "Assign an institution to each allocation using the PI's email"
+#(1) assign "lsu" to woodplc 
+#(2) assign "latech" to arprodserv
+#(3) assign "other" to missing values (NAs)
+#(4) freq table to ensure no more strange name exists, i.e.:
+#    table(alloc_w_org$org)  should only show 18 legitimate institutions 
+
+# 2. added a section "Get the city and state for each loni allocation."
+
+# 3. excluded "pi", "title" "description" and "sponsor" from alloc_w_org
+
+# 4. define allocation type (Research vs. Startup) in the alloc_w_org
+
+# 5. use the official name of the supercomputer in the jlog_w_org
+
+# 6. replace missing values in the research_area, email, org, system and region the jlog_w_org
+
+# 7. remove obs with years before 2013 in the alloc_w_org
+
+# 8. output file name change
+
+
 # Clean up job data.
 
 jlog <- jlog[complete.cases(jlog),]
@@ -33,24 +56,27 @@ if (prefix == "hpc_") {
 } else {
 
   # Clean up the PI affiliation for historical data.
-  # This part is hard coded.
+  # This part is hard coded. Check out the result from time to time. 
   # Legitimate institutions:
   # [1] "tulane"     "xula"       "subr"       "uno"        "latech"    
   # [6] "lsus"       "lsu"        "ulm"        "ull"        "nsula"
   #[11] "selu"       "lsuhsc"     "marybird"   "gram"       "lsmsa" 
   #[16] "mcneese"    "pbrc"       "other"
 
-  alloc_w_org$org <- gsub("(anl|army|arcadis-us|buffalo|colostate|indiana|caltech|seahorsecoastal|vims|sura|corning|iit|northeastern|auburn|lehigh|usf|inventherm|vcsu|umassd|gmail)","lsu",alloc_w_org$org)
+  alloc_w_org$org <- gsub("(anl|army|arcadis-us|buffalo|colostate|indiana|caltech|seahorsecoastal|vims|sura|corning|iit|northeastern|auburn|lehigh|usf|inventherm|vcsu|umassd|gmail|woodplc)","lsu",alloc_w_org$org)
   alloc_w_org$org <- gsub("wisc","subr",alloc_w_org$org)
   alloc_w_org$org <- gsub("(usach|dqsi|uark)","uno",alloc_w_org$org)
-  alloc_w_org$org <- gsub("(icnanotox|af|^uh$|famu|umass)","latech",alloc_w_org$org)
+  alloc_w_org$org <- gsub("(icnanotox|af|^uh$|famu|umass|arprodserv)","latech",alloc_w_org$org)
   alloc_w_org$org <- gsub("louisiana","ull",alloc_w_org$org)
   alloc_w_org$org <- gsub("sandia","tulane",alloc_w_org$org)
   alloc_w_org$org <- gsub("mpg","other",alloc_w_org$org)
+  alloc_w_org$org[is.na(alloc_w_org$org)]<-"other"
 
   alloc_w_org$org <- tolower(alloc_w_org$org)
-
 }
+
+# freq table to ensure no more strange name exists, i.e.: 
+table(alloc_w_org$org)  # should only show 18 legitimate institutions
 
 # Get the system for each allocation.
 
@@ -75,6 +101,28 @@ alloc_w_org$region <- mapvalues(alloc_w_org$org,
         to = c(rep("north",7),rep("south",10),"other")
 )
 
+# Get the city and state for each loni allocation.
+
+# Ruston: latech 
+# Shreveport: lsus
+# Monroe: ulm 
+# Natchitoches: nsula, lsmsa
+# New Orleans: lsuhsc, uno, tulane, xula
+# Grambling: gram, 
+# Baton Rouge: lsu, subr, pbrc, marybird
+# Lafayette: ull 
+# Hammond: selu 
+# Lake Charles: mcneese 
+# Grand Isle: other
+if (prefix == "loni_") {
+alloc_w_org$city <- mapvalues(alloc_w_org$org,
+        from = c("latech", "lsus", "ulm", "nsula", "lsmsa", "lsuhsc", "uno", "tulane", "xula", "gram", "subr",  "lsu", "pbrc", "marybird", "ull", "selu", "mcneese",  "other"),
+        to = c("Ruston","Shreveport","Monroe",rep("Natchitoches",2),rep("New Orleans",4),"Grambling",rep("Baton Rouge",4),"Lafayette","Hammond", "Lake Charles","Grand Isle")
+)
+
+alloc_w_org$state <- "Louisiana"
+} 
+
 # Assgin a discipline to each allocation.
 # 1: Physics
 # 2: Chemistry
@@ -91,6 +139,19 @@ alloc_w_org$area <- as.numeric(alloc_w_org$area1) %/% 100
 mapping = data.frame(key=c(1,2,3,4,5,6,7,8,9,10),area=c('Physics','Chemistry','Computer science and engineering','Biological sciences','Geosciences','Engineering','Material sciences',rep('Not specified',3)))
 alloc_w_org$research_area <- with(mapping,area[match(alloc_w_org$area,key)])
 
+# exclude "pi", "title" "description" and "sponsor" columns 
+alloc_w_org <- alloc_w_org[,-c(7,9,14,24)]
+
+# define allocation type
+alloc_w_org$type <- "Research"
+for(i in 1:nrow(alloc_w_org)){
+if (alloc_w_org$deposited[i] <= 50000) {
+
+  alloc_w_org$type[i] <- "Startup"
+  }
+}
+
+
 # Trim the allocation data frame.
 
 alloc_lean <- subset(alloc_w_org,select=c("project","org","email","system","region","research_area"))
@@ -100,3 +161,49 @@ alloc_lean <- alloc_lean[! duplicated(alloc_lean),]
 
 jlog_w_org <- merge(jlog,alloc_lean,by="project",all.x=T)
 jlog_w_org <- jlog_w_org[! duplicated(jlog_w_org),]
+
+
+# Use the official name of the supercomputer
+if (prefix == "hpc_") {
+
+  jlog_w_org$machine=gsub("Dell_Cluster","Tezpur",jlog_w_org$machine) 
+  jlog_w_org$machine=gsub("mike2","SuperMike-II",jlog_w_org$machine) 
+  jlog_w_org$machine=gsub("supermic.cct-lsu.xsede","SuperMIC",jlog_w_org$machine) 
+
+} else {
+
+  jlog_w_org$machine=gsub("Dell_Cluster","Eric and others",jlog_w_org$machine) 
+  jlog_w_org$machine=gsub("p5-575","Not specified",jlog_w_org$machine) 
+  jlog_w_org$machine=gsub("queenbee.loni-lsu.teragrid","Queenbee2",jlog_w_org$machine) 
+}
+
+# Replace missing values in the research_area, email, org, system and region
+
+jlog_w_org$research_area[is.na(jlog_w_org$research_area)]<-"Not specified"
+jlog_w_org$email[is.na(jlog_w_org$email)]<-"Not specified"
+jlog_w_org$org[is.na(jlog_w_org$org)]<-"Not specified"
+jlog_w_org$system[is.na(jlog_w_org$system)]<-"Not specified"
+jlog_w_org$region[is.na(jlog_w_org$region)]<-"Not specified"
+
+# Remove observations with years before 2013
+
+alloc_w_org <- alloc_w_org[alloc_w_org$year > 2012,]
+jlog_w_org <- jlog_w_org[jlog_w_org$year > 2012,]
+
+# Dump the allocation and job data frame into CSV files as the data source for  other applications such as Tableau.
+cdate <- gsub("-","",as.character(today()))
+fprefix <- gsub("hpc","lsu",prefix)
+
+# allocfile and jobfile are for back-up with date in the file name
+# allocfile2 and jobfile2 are for use in Tableau (unique name w/o date)
+allocfile <- paste0(toupper(fprefix),"HPC_allocation_log.",cdate,"_clean",".csv")
+allocfile2 <- paste0(toupper(fprefix),"HPC_allocation_log","_clean",".csv")
+jobfile <- paste0(toupper(fprefix),"HPC_job_log.",cdate,"_clean",".csv")
+jobfile2 <- paste0(toupper(fprefix),"HPC_job_log","_clean",".csv")
+
+write.csv(alloc_w_org,file=allocfile,row.names=F,quote=F)
+write.csv(alloc_w_org,file=allocfile2,row.names=F,quote=F)
+write.csv(jlog_w_org,file=jobfile,row.names=F,quote=F)
+write.csv(jlog_w_org,file=jobfile2,row.names=F,quote=F)
+
+
